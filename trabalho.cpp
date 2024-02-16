fe68b0cd384d199294ade1256ac00c8198ee28f0
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-16 09:50:57 -0100

    updates

--- trabalho.cpp
+++ trabalho.cpp
@@ -5,18 +5,25 @@ int main(){
 
 int a;
 int b;
+const double PI = 3.14159;
+int r;
+int area;
 
     cout << "Introduza o primeiro valor: ";
     cin >> a;
     cout << "Introduza o segundo valor: ";
     cin >> b;
 
-    cout << "A soma: " << a + b << endl;
-    cout << "A diferença: " << a - b << endl;
-    cout << "O produto: " << a * b << endl;
-    cout << "A divisão: " << a / b << endl;
-    cout << "O resto da divisão: " << a % b << endl;
+        cout << "A soma: " << a + b << endl;
+        cout << "A diferença: " << a - b << endl;
+        cout << "O produto: " << a * b << endl;
+        cout << "A divisão: " << a / b << endl;
+        cout << "O resto da divisão: " << a % b << endl;
 
+    cout << "Defina o valor de 'r': ";
+    cin >> r;
+    area = PI * r * r;
+        cout << "A área do circulo é: " << area << endl;
 
     return 0;
 }
