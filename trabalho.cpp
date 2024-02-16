ed34102d51c0cb5f734f8680dec68f55c53b6e60
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-16 09:45:40 -0100

    updates

--- trabalho.cpp
+++ trabalho.cpp
@@ -6,9 +6,16 @@ int main(){
 int a;
 int b;
 
-    cout << "Introduza os dois valores: " << endl;
+    cout << "Introduza o primeiro valor: ";
     cin >> a;
+    cout << "Introduza o segundo valor: ";
     cin >> b;
+
+    cout << "A soma: " << a + b << endl;
+    cout << "A diferença: " << a - b << endl;
+    cout << "O produto: " << a * b << endl;
+    cout << "A divisão: " << a / b << endl;
+    cout << "O resto da divisão: " << a % b << endl;
 
 
     return 0;
