9d9a483bed8379bee7b19775a6328f6e16f39108
telma <145616594+tsrebelo@users.noreply.github.com>
2024-02-18 17:16:03 -0100

    updates

--- trabalho.cpp
+++ trabalho.cpp
@@ -39,12 +39,26 @@ int main(){
 
 //Parte 2
 //Arrays unidimensional
-float notas[5] = {1 ,2 ,3 ,4 ,5};
+float notas[5];
+float min = 0;
+float max = 0;
 
 int main(){
+   
+    cout << "Introduza as notas dos alunos." << endl;
+    cout << "Aluno 1: ";
+    cin >> notas[0];
+    cout << "Aluno 2: ";
+    cin >> notas[1];
+    cout << "Aluno 3: ";
+    cin >> notas[2];
+    cout << "Aluno 4: ";
+    cin >> notas[3];
+    cout << "Aluno 5: ";
+    cin >> notas[4];
 
-    for(int i = 0; i < 5; i++){
-        cout << notas[i] << " ";
+    for(int x = 0; x < 1; x++){
+        cout << "A média das notas é: " << (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / 5 << endl;
     }
 
 }
