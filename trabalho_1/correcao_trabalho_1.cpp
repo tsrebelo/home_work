363c30161825154c67054454121e0a5520aceb9e
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-20 09:32:00 -0100

    updates

---
+++ trabalho_1/correcao_trabalho_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+long a,b;
+const double PI = 3.14159265358979323846;
+int myArray[5];
+
+int main(){
+
+//CALCULADORA
+    cout << "Digite o valor de a: ";
+    cin >> a;
+    cout << "Digite o valor de b: ";
+    cin >> b;
+    cout << "A soma de a e b é: " << a+b << endl;
+    cout << "A subtração de a e b é: " << a-b << endl;
+    cout << "A multipicação de a e b é: " << a*b << endl;
+    if(b != 0){
+        cout << "A divisão de a e b é: " << a/b << endl;
+    }else{
+        double c = a/(double)b;
+        cout << "Não é possível dividir por zero!" << endl;
+        cout << "O resto da divisão de a e b é: " << a%b << endl;
+    }
+
+
+//CÁLCULO DA ÁREA
+
+    return 0;
+}
